bd9aff6ca63d2a0480b85921224601dccd765f95
wyork <w.york63@gmail.com>
2022-02-04 16:35:30 -0500

    update intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,21 @@
-const { TestWatcher } = require('jest');
 const Intern = require('../lib/Intern');
 
 // creates intern object
-TestWatcher('creates an Intern object', () => {
-    const intern = new Intern ('Will', 124, )
-})
+test('creates an Intern object', () => {
+    const intern = new Intern ('Will', 124, 'w.york63@gmail.com', 'VT');
+
+    expect(intern.name).toEqual(expect.any(String));
+});
+
+// gets from get school
+test('gets employee school', () => {
+    const intern = new Intern('Will', 124, 'w.york63@gmail.com', 'VT');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('gets role of employee', () => {
+    const intern = new Intern ('Will', 124, 'w.york63@gmail.com', 'VT');
+
+    expect(intern.getRole()).toEqual("Intern");
+});
